d0c49ad034f5fee093aac19a37d08e5554608fdf
RedC0mrade <e2e4e2e4@mail.ru>
2025-01-04 15:23:56 +0200

    alembic env

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from app.core.models.base_model import Base
+from app.core.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -28,7 +29,7 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
+config.set_main_option("sqlalchemy.url", str(settings.db.url))
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
