000c786c75b0cbeb35b95eaf81b19695c970e51a
RedC0mrade <e2e4e2e4@mail.ru>
2025-03-24 21:29:42 +0200

    acnions create test users

--- actions/create_test_users.py
+++ actions/create_test_users.py
@@ -1,34 +1,68 @@
 import asyncio
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
+import contextlib
+from fastapi_users.exceptions import UserAlreadyExists
 
-from app.core.models.user import UserAlchemyModel
-from app.core.models import db_helper
+from app.api.dependencies.authentication import get_users_db
+from app.api.dependencies.authentication import get_user_manager
+from app.core.auth.schemas import UserCreate
+from app.core.auth.user_manager import UserManager
+
+from app.core.models import (
+    db_helper,
+    UserAlchemyModel,
+)
 
 
-async def create_test_users(
-    session: AsyncSession = Depends(db_helper.session_getter),
+get_users_db_context = contextlib.asynccontextmanager(get_users_db)
+get_user_manager_context = contextlib.asynccontextmanager(get_user_manager)
+
+default_username = ("first", "second")
+default_email = ("first@first.com", "second@second.com")
+default_password = "111"
+default_is_active = True
+default_is_superuser = False
+default_is_verified = True
+
+
+async def create_user(
+    user_manager: UserManager,
+    user_create: UserCreate,
+) -> UserAlchemyModel:
+    user = await user_manager.create(
+        user_create=user_create,
+        safe=False,
+    )
+    return user
+
+
+async def create_users(
+    username: list = default_username,
+    email: list = default_email,
+    password: str = default_password,
+    is_active: bool = default_is_active,
+    is_superuser: bool = default_is_superuser,
+    is_verified: bool = default_is_verified,
 ):
-    stmt = [
-        UserAlchemyModel(
-            username="first",
-            email="first@first.com",
-            hashed_password="111",
-        ),
-        UserAlchemyModel(
-            username="second",
-            email="second@second.com",
-            hashed_password="111",
-        ),
-    ]
-    session.add_all(stmt)
-    await session.commit()
-
-
-async def main():
-    async with db_helper.session_factory() as session:
-        await create_test_users(session=session)
+    for value in range(len(username)):
+        user_create = UserCreate(
+            username=username[value],
+            email=email[value],
+            password=password,
+            is_active=is_active,
+            is_superuser=is_superuser,
+            is_verified=is_verified,
+        )
+        async with db_helper.session_factory() as session:
+            async with get_users_db_context(session) as users_db:
+                async with get_user_manager_context(users_db) as user_manager:
+                    await create_user(
+                        user_manager=user_manager,
+                        user_create=user_create,
+                    )
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(create_users())
+    except UserAlreadyExists:
+        raise
