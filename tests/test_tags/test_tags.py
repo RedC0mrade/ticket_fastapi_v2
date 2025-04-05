df3b57c617bcfdb8a30e4bc7726809f9cdaefeea
RedC0mrade <e2e4e2e4@mail.ru>
2025-04-05 20:23:21 +0200

    test tag_validations

--- tests/test_tags/test_tags.py
+++ tests/test_tags/test_tags.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.schemas.tag import Tag
 from app.crud.tags import TagService
+from app.core.schemas.tag import CreateTag
 
 
 class TestTagsService:
@@ -16,7 +17,7 @@ class TestTagsService:
         self,
         session: AsyncSession,
         tag_white: Tag,
-        tag_black,
+        tag_black: Tag,
     ):
         self.session = session
         self.tag_white = tag_white
@@ -37,4 +38,21 @@ class TestTagsService:
         assert tags == list()
 
     async def test_create_tags(self):
-        
+        tags = [
+            CreateTag(tag_name="Rose", tag_color="#000002"),
+            CreateTag(tag_name="Grey", tag_color="#000003"),
+        ]
+        await self.tag_service.create_tags(tags_in=tags)
+        tags_in_base = await self.tag_service.get_all_tags()
+
+        assert len(tags_in_base) == 4
+        assert tags_in_base[2].tag_name == "Rose"
+        assert tags_in_base[3].tag_name == "Grey"
+
+    async def test_delete_tag(self):
+        tags_in_base = await self.tag_service.get_all_tags()
+        assert len(tags_in_base) == 2
+        await self.tag_service.delete_tag(tag_id=self.tag_white.id)
+        tags_in_base = await self.tag_service.get_all_tags()
+        assert len(tags_in_base) == 1
+        assert tags_in_base[0].tag_name == "Black"
